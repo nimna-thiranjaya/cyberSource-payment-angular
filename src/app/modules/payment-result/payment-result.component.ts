6f645afac902b5c3904f268a67ff4dda5bac0643
nimna-thiranjaya <nimnathiranjaya523@gmail.com>
2023-09-09 16:14:04 +0530

    ui changes

--- src/app/modules/payment-result/payment-result.component.ts
+++ src/app/modules/payment-result/payment-result.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-payment-result',
   templateUrl: './payment-result.component.html',
-  styleUrls: ['./payment-result.component.scss']
+  styleUrls: ['./payment-result.component.scss'],
 })
 export class PaymentResultComponent {
-
+  ngOnInit(): void {
+    console.log('PaymentResultComponent');
+  }
 }
